18bded58c333e858545c125fe6a102ed0825ed9e
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 17:45:20 -0700

    remove mroe template code

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
       <p>SiPPP is the Simple Photo Provenance Protocol.</p>
       <a href="https://sippp.box">Learn More</a>
       <p>This is a Farcaster action for verifying that a photo was taken with a camera and not altered.</p>
-      <a href="https://warpcast.com/~/add-cast-action?actionType=post&name=Verify%20with%20SiPPP&icon=verified&postUrl=https%3A%2F%2Fsi-ppp-action.vercel.app%2Fapi%2Faction">Install the Action:</a>
+      <a href="https://warpcast.com/~/add-cast-action?actionType=post&name=Verify%20with%20SiPPP&icon=verified&postUrl=https%3A%2F%2Fsi-ppp-action.vercel.app%2Fapi%2Faction">Install the Action</a>
     </>
   );
 }
