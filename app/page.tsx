9035e54e1daef0a096b03cdb6beec3ef0a1812a2
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 17:36:13 -0700

    more on front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import { getFrameMetadata } from '@coinbase/onchainkit/frame';
-import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from './config';
-
-
 export default function Page() {
   return (
     <>
-      <h1>SiPPP Farcaster Action</h1>
+      <h1>Verify with SiPPP</h1>
+      <p>SiPPP is the Simple Photo Provenance Protocol.</p>
+      <a href="https://sippp.box">Learn More</a>
+      <p>This is a Farcaster action for verifying that a photo was taken with a camera and not altered.</p>
+      <a href="https://warpcast.com/~/add-cast-action?actionType=post&name=Verify%20with%20SiPPP&icon=verified&postUrl=https%3A%2F%2Fsi-ppp-action.vercel.app%2Fapi%2Faction">Install the Action:</h2>
     </>
   );
 }
